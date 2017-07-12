9eb53e464b281bf66de42081b802dabfef7654bc
Samuel Gomez <gressboldude@gmail.com>
2017-07-12 19:22:52 +0200

    remove functions

--- todolist-project/assets/js/scripts.js
+++ todolist-project/assets/js/scripts.js
@@ -1,4 +1,16 @@
 
+
+// toggle  text overline
 $("li").click(function(){
 	$(this).toggleClass("completed")
+});
+
+// delete text event
+$("span").click(function(e){
+	$(this).parent().fadeOut(400, function(){
+		$(this).remove();
+	});
+
+	// stops the event from affection parents
+	e.stopPropagation();
 });
