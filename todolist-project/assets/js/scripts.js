083cd43cd1069c15d5f5775dae1c4544f04c12b1
Samuel Gomez <gressboldude@gmail.com>
2017-07-13 13:53:29 +0200

    finnished todo

--- todolist-project/assets/js/scripts.js
+++ todolist-project/assets/js/scripts.js
@@ -1,16 +1,32 @@
 
 
 // toggle  text overline
-$("li").click(function(){
+	// since code is read sync it can only select elements that exists when the code is read
+	// so make the event listen to changes of elements within ań element that already exists when the code is read
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed")
 });
 
 // delete text event
-$("span").click(function(e){
+$("ul").on("click", "span", function(e){
 	$(this).parent().fadeOut(400, function(){
 		$(this).remove();
 	});
 
 	// stops the event from affection parents
 	e.stopPropagation();
+});
+
+// input
+$("input[type='text']").keypress(function(e){
+	if (e.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>");
+	};
+});
+
+// input toggle 
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
 });
